48ace5ee9c54170f85664927d184e49e927a7554
Daniel Rodriguez <daniel@danielrodriguez.name>
2020-06-23 13:58:22 -0400

    builds header and footer dynamically from a single data source.

--- js/header_footer.js
+++ js/header_footer.js
@@ -1,54 +1,65 @@
 
-var header = `
-<div id=\"logo\"> 
+var data = [
+    {
+        active: true,
+        href: "index.html",
+        span: "Home"
+    },
+    {
+        active: true,
+        href: "resume.html",
+        span: "Resume"
+    },
+    {
+        active: true,
+        href: "portfolio.html",
+        span: "Portfolio"
+    },
+    {
+        active: false,
+        href: "blog.html",
+        span: "Blog"
+    },
+    {
+        active: true,
+        href: "contact.html",
+        span: "Contact"
+    }
+];
+
+var divLogo = `
+<div id="logo"> 
     <h1>
         <a href="index.html">Daniel Rodriguez</a></h1> 
     <p>A game/software/entertainment engineer</p> 
-</div> 
-<div id="tabs">
-    <ul>
-        <li><a href="index.html">
-            <span>Home</span>
-        </a></li>
-        <li><a href="resume.html">
-                <span>Resume</span>
-        </a></li>
-        <li><a href="portfolio.html">
-                <span>Portfolio</span>
-        </a></li>
-        <!--li>
-            <a href="blog.html">Blog</a>
-        </li-->
-        <li><a href="contact.html">
-                <span>Contact</span>
-        </a></li>
-    </ul>
 </div>`;
 
-var footer = `
-<ul class="links">
-    <li>
-        <a href="index.html">Home</a>
-    </li>
-    <li>
-        <a href="resume.html">Resume</a>
-    </li>
-    <li>
-        <a href="portfolio.html">Portfolio</a>
-    </li>
-    <!--li>
-        <a href="blog.html">Blog</a>
-    </li-->
-    <li>
-        <a href="contact.html">Contact</a>
-    </li>
-</ul>
-`;
-
 function loadHeader() {
-    $('div#topPan').append(header);
+    var divTabs = $('<div />' ).attr("id", "tabs").append("<ul />"); 
+
+    $('div#topPan').append(divLogo);
+    $('div#topPan').append(divTabs);
+
+    data.forEach(datum => {
+        if (datum.active) {
+            var span = $(`<span>${datum.span}</span>`)
+            var anchor = $("<a />").attr("href", datum.href).append(span)
+            var listItem = $( "<li />" ).append(anchor)
+            $('div#tabs ul').append(listItem)
+        }
+    });
 }
 
 function loadFooter() {
-    $('div#footerPan').prepend(footer);
+    var ul = $('<ul />').addClass("links")
+    
+    $('div#footerPan').prepend(ul);
+
+    data.forEach(datum => {
+        if (datum.active) {
+            var anchor = $("<a />").attr("href", datum.href).html(datum.span)
+            var listItem = $( "<li />" ).append(anchor)
+            $('div#footerPan ul.links').append(listItem)
+        }
+    });
 }
